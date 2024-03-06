d48125900a58e8d317928a0605c21858025a60e3
weijiefu1673 <74281897+weijiefu1673@users.noreply.github.com>
2024-03-07 02:20:26 +0800

    Add files via upload

---
+++ ipq6000/settings.6.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+
+#sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
