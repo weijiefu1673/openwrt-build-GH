ad71e6ab910e4ed298b85e9694d4f2dc8278570e
weijiefu1673 <74281897+weijiefu1673@users.noreply.github.com>
2024-03-18 21:15:36 +0800

    Update breeze303_6.1_packages.sh

--- ipq60xx/breeze303_6.1_packages.sh
+++ ipq60xx/breeze303_6.1_packages.sh
@@ -8,7 +8,9 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 
 git clone --depth=1 --branch main https://github.com/weijiefu1673/openwrt_package openwrt_package
-mv -f openwrt_package/* package/emortal
+find openwrt_package -type f -exec chmod 755 {} \;
+rm -rf package/emortal/cpufreq
+mv openwrt_package/* package/emortal
 
 mv ../ipq60xx/0001-show-soc-status-on-luci.patch feeds/luci
 cd feeds/luci
