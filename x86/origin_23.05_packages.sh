bb9083a2854208305bc3ab087357c9f0913dc99b
weijiefu1673 <74281897+weijiefu1673@users.noreply.github.com>
2024-03-19 23:48:39 +0800

    Add files via upload

---
+++ x86/origin_23.05_packages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+
+git clone --depth=1 --branch main https://github.com/weijiefu1673/openwrt_package openwrt_package
+unzip openwrt_package/luci-app-daily.zip -d openwrt_package/
+find openwrt_package -type f -exec chmod +x {} \;
+mv openwrt_package/* package/emortal
---
+++ x86/origin_23.05_settings.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+
+sed -i 's/0.openwrt.pool.ntp.org/ntp.tencent.com/g' package/base-files/files/bin/config_generate
+sed -i 's/1.openwrt.pool.ntp.org/ntp1.aliyun.com/g' package/base-files/files/bin/config_generate
+sed -i 's/2.openwrt.pool.ntp.org/ntp.ntsc.ac.cn/g' package/base-files/files/bin/config_generate
+sed -i 's/3.openwrt.pool.ntp.org/cn.ntp.org.cn/g' package/base-files/files/bin/config_generate
