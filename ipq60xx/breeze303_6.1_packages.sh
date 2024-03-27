e443a808396ac6a7c426e7a1576440fd0d754d16
weijiefu1673 <74281897+weijiefu1673@users.noreply.github.com>
2024-03-28 01:04:19 +0800

    Update breeze303_6.1_packages.sh

--- ipq60xx/breeze303_6.1_packages.sh
+++ ipq60xx/breeze303_6.1_packages.sh
@@ -12,8 +12,8 @@ find openwrt_package -type f -exec chmod +x {} \;
 rm -rf package/emortal/cpufreq
 mv openwrt_package/* package/emortal
 
-mv ../ipq60xx/0001-show-soc-status-on-luci.patch feeds/luci
+mv ../ipq60xx/0002-show-soc-status-on-luci.patch feeds/luci
 cd feeds/luci
 git config --global user.email "888@888.com"
 git config --global user.name "888"
-git am 0001-show-soc-status-on-luci.patch
+git am 0002-show-soc-status-on-luci.patch
