9b5d112df19b35b42912d539d33c97e66bfbff83
weijiefu1673 <74281897+weijiefu1673@users.noreply.github.com>
2025-03-19 00:50:05 +0800

    Add files via upload

---
+++ mt7621/lean_packages.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+
+
